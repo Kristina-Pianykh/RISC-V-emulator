ed6cf57722b202ec408afede7e641c292825d5da
Kristina Pianykh <kristinavrnrus@gmail.com>
2022-08-27 03:55:20 +0200

    chore: align opcodes

--- main.c
+++ main.c
@@ -8,56 +8,56 @@
 #define reg cpu->regfile_ //37 instructions
 #define pc cpu->pc_
 
-#define LUI   0x37
-#define AUIPC 0x17
-#define JAL   0x6F
-#define JALR  0x67
+#define LUI   			0x37
+#define AUIPC 			0x17
+#define JAL   			0x6F
+#define JALR  			0x67
 
-#define I     0x13
-    #define ADDI    0x0
-	#define SLTI    0x2
-    #define SLTIU   0x3
-    #define XORI    0x4
-    #define ORI     0x6
-    #define ANDI    0x7
-    #define SLLI    0x1
-    #define SRI     0x5
-        #define SRLI    0x00
+#define I     			0x13
+    #define ADDI    	0x0
+	#define SLTI    	0x2
+    #define SLTIU   	0x3
+    #define XORI    	0x4
+    #define ORI     	0x6
+    #define ANDI    	0x7
+    #define SLLI    	0x1
+    #define SRI     	0x5
+        #define SRLI	0x00
         #define SRAI    0x20
 
-#define R     0x33
-	#define ADD_SUB 0x0
-		#define ADD 0x0
-		#define SUB 0x2
-	#define SLL     0x1
-	#define SLT     0x2
-	#define SLTU    0x3
-	#define XOR     0x4
-	#define SR      0x5
+#define R     			0x33
+	#define ADD_SUB 	0x0
+		#define ADD 	0x0
+		#define SUB 	0x2
+	#define SLL     	0x1
+	#define SLT     	0x2
+	#define SLTU    	0x3
+	#define XOR     	0x4
+	#define SR      	0x5
 		#define SRL     0x0
 		#define SRA     0x2
-	#define OR      0x6
-	#define AND     0x7
+	#define OR      	0x6
+	#define AND     	0x7
 
-#define S     0x23
-	#define SB      0x0
-	#define SH      0x1
-	#define SW		0x2
+#define S     			0x23
+	#define SB      	0x0
+	#define SH      	0x1
+	#define SW			0x2
 
-#define L     0x03
-	#define LHU     0x5
-	#define LB      0x0
-	#define LH      0x1
-	#define LW      0x2
-	#define LBU     0x4
+#define L     			0x03
+	#define LHU     	0x5
+	#define LB      	0x0
+	#define LH      	0x1
+	#define LW      	0x2
+	#define LBU     	0x4
 	
-#define B     0x63
-	#define BEQ     0x0
-	#define BNE     0x1
-	#define BLT     0x4
-	#define BGE     0x5
-	#define BLTU    0x6
-	#define BGEU    0x7
+#define B     			0x63
+	#define BEQ     	0x0
+	#define BNE     	0x1
+	#define BLT     	0x4
+	#define BGE     	0x5
+	#define BLTU    	0x6
+	#define BGEU    	0x7
 
 typedef struct
 {
